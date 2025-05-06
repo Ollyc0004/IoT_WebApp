706189ec81ff523e7c06c57aaa92e7b96c4deaa8
COOK Oliver <oliver.cook@airbus.com>
2025-05-06 21:26:38 +0100

    V 0.1.8

---
+++ connect.php
@@ -0,0 +1,26 @@
+<?php
+$server ="plesk.remote.ac";
+$username = "WS371632_IoT";
+$password = "!v65Rny56";
+$database = "WS371632_IoT";
+
+$connect = mysqli_connect($server,$username,$password,$database);
+
+
+function runAndCheckSQL($connection, $sql){
+    $run = mysqli_query($connection, $sql);
+    if ($run) {
+        if(is_array($run) || is_object($run)){
+            return $run;
+        }else{
+            return true;
+        }
+    } else {
+        die(showError($sql, $connection));
+    }
+}
+
+function showError($sql, $connection){
+    echo "<div class=\"alert alert-danger\"><strong>ERROR!</strong> : " .  $sql . "<br>" . mysqli_error($connection)."</div>";
+}
+?>
