19a27cfea64f451a4d1a06afb3ad0a321ac244a6
COOK Oliver <oliver.cook@airbus.com>
2025-05-06 16:09:52 +0100

    v0.1

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php 
+//Database credentials to connect to the database
+$servername = "plesk.remote.ac";
+$database = "WS371632_IoT";
+$username = "WS371632_IoT";
+$password = "!v65Rny56";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+?>
